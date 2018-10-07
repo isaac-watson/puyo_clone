1acef7bfa72177df5479265f6340ea7bb4e86ba0
Isaac Watson <iawatson@oakland.edu>
2018-10-07 18:10:24 -0400

    Basic classes

--- Puyo Clone/Assets/Scripts/Puyo.cs
+++ Puyo Clone/Assets/Scripts/Puyo.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Puyo : MonoBehaviour {
+public class Puyo {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum PuyoColor { RED, BLUE, YELLOW, GREEN, PURPLE };
+    private PuyoColor puyoColor;
+    
+    //Position with respect to board puyo array
+    private int x, y;
+    
+    public int X
+    {
+        get { return x; }
+        set { x = value; }
+
+    }
+    public int Y
+    {
+        get { return y; }
+        set { y = value; }
+    }
+
+    public PuyoColor Color
+    {
+        get { return puyoColor; }
+        set { puyoColor = value; }
+    }
+
+	public Puyo(PuyoColor puyoColor)
+    {
+        x = 3;
+        y = 0;
+        this.puyoColor = puyoColor;
+    }
+
+
+    
 }
